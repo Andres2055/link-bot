f3d0574bb9f10b427f89c1e85c10b1eee9d2fac1
Andrés <andresecm01@gmail.com>
2018-12-24 16:12:18 -0400

    Update info.js

--- Commands/info.js
+++ Commands/info.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args) => {
 	const embed = new Discord.RichEmbed()
 		.setURL('https://github.com/Andres2055/link-bot')
 		.setDescription("¡Hola! Soy Estiben, probablemente alguna proyección de algún sujeto en un universo paralelo o algo.")
-		.setAuthor("Estiben (ver 0.3)", client.user.displayAvatarURL)
+		.setAuthor("Estiben (ver 0.4.1)", client.user.displayAvatarURL)
 		.setThumbnail("https://media.discordapp.net/attachments/520481910397468685/521122008369594405/Ball3.jpg")
 		.addField('Gustos', "Cuervos y Consomé Panchi", true)
 		.addField('Hobbys', "Buscar enlaces, escribir y ser tu esclavo", true)
