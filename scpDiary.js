e2db4dab04b9496114b10f43f544c3292b56f858
Andrés <andresecm01@gmail.com>
2018-12-24 16:02:18 -0400

    Add files via upload

---
+++ scpDiary.js
@@ -0,0 +1,88 @@
+const Discord = require('discord.js');
+const Scpper = require("scpper.js");
+const all = require('./Commands/utils/allUNeed.js');
+
+const api = new Scpper.Scpper({site: 'es'});
+const date = new Date();
+
+var getChannel = (client) => {
+	try {
+		const guild = client.guilds.find(guild => guild.id === '526262811471511572');
+		const channel = guild['channels'].find(ch => ch.id === '526262811471511574');
+		return channel;
+	} catch(err) {
+		console.log('Aún no se ha logeado');
+	}
+}
+
+module.exports.postSCPDiary = client => {
+	const channel = getChannel(client);
+	if(date.getUTCHours() == 12) {
+		channel.send('**Estas son las recomendaciones del día:**');
+
+		scpDiary = api.findTag('scp', { random: true });
+		scpESDiary = api.findTag(['+scp', '+es'], { random:true });
+		taleDiary = api.findTag('relato', { random:true });
+		taleESDiary = api.findTag(['+relato', '+es'], { random:true });
+
+		scpDiary.then(function(value) {
+			page = value['data']['pages'][0];
+
+			const embed = new Discord.RichEmbed()
+				.setTitle(all.checkTitle(page['title'], page['altTitle']) + ' (' + all.checkVotes(page['rating']) + ')')
+				.setURL(page['site'] + '\/' + page['name'])
+				.setDescription(all.checkAuthors(page['status'], page['authors']))
+				.setAuthor('SCP del Día')
+				.setColor(0x588d9b);
+
+			channel.send({ embed });
+		})
+		.catch(err => console.log('scpDiary' + err));
+
+		scpESDiary.then(function(value) {
+			page = value['data']['pages'][0];
+						console.log(page)
+
+
+			const embed = new Discord.RichEmbed()
+				.setTitle(all.checkTitle(page['title'], page['altTitle']) + ' (' + all.checkVotes(page['rating']) + ')')
+				.setURL(page['site'] + '\/' + page['name'])
+				.setDescription(all.checkAuthors(page['status'], page['authors']))
+				.setAuthor('SCP-ES del Día')
+				.setColor(0x588d9b);
+
+			channel.send({ embed });
+		})
+		.catch(err => console.log('scpESDiary' + err));
+
+		taleDiary.then(function(value) {
+			page = value['data']['pages'][0];
+
+			const embed = new Discord.RichEmbed()
+				.setTitle(all.checkTitle(page['title'], page['altTitle']) + ' (' + all.checkVotes(page['rating']) + ')')
+				.setURL(page['site'] + '\/' + page['name'])
+				.setDescription(all.checkAuthors(page['status'], page['authors']))
+				.setAuthor('Relato del Día')
+				.setColor(0x588d9b);
+
+			channel.send({ embed });
+		})
+		.catch(err => console.log('taleDiary' + err));
+
+		taleESDiary.then(function(value) {
+			page = value['data']['pages'][0];
+
+			const embed = new Discord.RichEmbed()
+				.setTitle(all.checkTitle(page['title'], page['altTitle']) + ' (' + all.checkVotes(page['rating']) + ')')
+				.setURL(page['site'] + '\/' + page['name'])
+				.setDescription(all.checkAuthors(page['status'], page['authors']))
+				.setAuthor('Relato-ES del Día')
+				.setColor(0x588d9b);
+
+			channel.send({ embed });
+		})
+		.catch(err => console.log('taleESDiary' + err));
+	} else {
+		console.log('Aún no es hora');
+	};
+};
