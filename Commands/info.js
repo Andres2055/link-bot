dc376806b4fce25e0842b6666a735417a35df372
Andrés <andresecm01@gmail.com>
2018-12-24 16:34:51 -0400

    Update info.js

--- Commands/info.js
+++ Commands/info.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) => {
 		.setURL('https://github.com/Andres2055/link-bot')
 		.setDescription("¡Hola! Soy Estiben, probablemente alguna proyección de algún sujeto en un universo paralelo o algo.")
 		.setAuthor("Estiben (ver 0.4.1)", client.user.displayAvatarURL)
-		.setThumbnail("https://media.discordapp.net/attachments/520481910397468685/521122008369594405/Ball3.jpg")
+		.setThumbnail("https://media.discordapp.net/attachments/319248418952445953/523682038373548043/ESTIBENXMAS.jpg")
 		.addField('Gustos', "Cuervos y Consomé Panchi", true)
 		.addField('Hobbys', "Buscar enlaces, escribir y ser tu esclavo", true)
 		.setColor(0x588d9b)
