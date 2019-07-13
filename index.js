6e5551c83776fa321ad69f5e284718e68dc2ed64
Andrés <andresecm01@gmail.com>
2019-07-13 17:41:58 -0400

    bug fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require('fs');
+const antispam = require("antispam-discord")
 const config = require("./Storage/config.json");
 
 /* Deviroment Variables */
@@ -33,9 +34,25 @@ fs.readdir("./Commands/", (err, files) => {
 	});
 });
 
-
-
 /* Commands */
+/* Anti Spam */
+
+/*let cooldown = {};
+
+setInterval(() => {
+	if(!cooldown) return;
+	for(let x in cooldown) {
+		now = Date.now()
+		console.log(">>>", cooldown)
+		console.log(cooldown[x][1] - 30000)
+		console.log((cooldown[x][1] - 30000) < now)
+		if((cooldown[x][1] - 30000) < now) {
+			cooldown[x][1] = 0;
+		}
+	}
+}, 10000)*/
+
+/* Anti Spam */
 /* Client */
 
 client.on("ready", () => {
@@ -70,25 +87,30 @@ client.on('guildMemberAdd', member => {
 client.on('guildMemberRemove', member => {
 	const channel = member.guild.channels.find(ch => ch.name === 'lobby');
 	if (!channel) return;
-	channel.send(`¡Adios, ${member}! Espero que vuelvas pronto :D.`);
+	channel.send(`¡Adios, ${member}! Espero que vuelvas pronto :D`);
 });
 
 client.on('guildBanAdd', member => {
-	const channel = member.guild.channels.find(ch => ch.name === 'staff');
+	const channel = member.guild.channels.find(ch => ch.name === 'entradas');
 	if (!channel) return;
-	channel.send(`${member}. Dios, no... pensé que realmente eras una buena persona, y ser baneado... supongo que no todas son buenas personas.`);
+	channel.send(`${member.name} ha sido baneado`);
 });
 
-/*client.on('messageDelete', msg => {
-	const channel = member.guild.channels.find(ch => ch.name === 'staff');
-	if (!channel) return;
-	channel.send(`${member} ha sido baneado del servidor.`);
-});*/
-
 client.on("message", message => {
-	const isMuted = message.member.roles.find(rol => rol.name.toLowerCase() == "muted")
-	if (isMuted) {
-		message.delete();
+	console.log(message.channel.type)
+	if(message.channel.type != "dm") {
+		const isMuted = message.member.roles.find(rol => rol.name.toLowerCase() == "muted")
+		if (isMuted) {
+			message.delete()
+				.then(msg => console.log(`Mensaje de ${msg.author.username} borrado`))
+				.catch(console.error)
+			return;
+		}
+
+		let adv_roles = {}
+		adv_roles["n1"] = message.member.guild.roles.find(rol => rol.name === 'Advertencia de Nivel 1')
+		adv_roles["n2"] = message.member.guild.roles.find(rol => rol.name === 'Advertencia de Nivel 2')
+		adv_roles["muted"] = message.member.guild.roles.find(rol => rol.name === 'Muted')
 	}
 
 	var msgR = () => { //%10 de que salga
@@ -125,29 +147,72 @@ client.on("message", message => {
 		}
 	}
 
+	var checkIt = (to_check_cmd) => {
+		const alert = ["scp", "scpr", "user", "autor", "art", "artr", "tag", "etiq"]
+		for (let i = 0; i < alert.length; i++) {
+			if(to_check_cmd == alert[i]) {
+				return true;
+			}
+		}
+	}
+
+	var isOcio = (ocio_cmd) => {
+		const alert = ["bola8", "b8", "8b", "img", "f"]
+		for (let i = 0; i < alert.length; i++) {
+			if(ocio_cmd == alert[i]) {
+				return true;
+			}
+		}
+	}
+
 	if (!message.author.bot) {
 		let messageArray = message.content.split(/ +/g);
 		let cmd = messageArray[0].toLowerCase();
 		let args = messageArray.slice(1)
 		if (!message.content.startsWith(PREFIX)) return;
+		/*if(isOcio(cmd.slice(PREFIX.length)) && message.channel.type != "dm") {
+			if(cooldown[message.author.id]) {
+				cooldown[message.author.id][0] += 1;
+			} else if(!cooldown[message.author.id]) {
+				cooldown[message.author.id] = [1, 0, 0]
+			}
+
+			console.log("------------\n", cooldown)
+
+
+			if(cooldown[message.author.id][0] == 10) {
+				console.log("¡shut up!")
+				cooldown[message.author.id][0] = 0;
+				cooldown[message.author.id][1] = Date.now() + 30000;
+				cooldown[message.author.id][2] += 1;
+				console.log(cooldown)
+				if(cooldown[message.author.id][2] == 3) {
+					message.member.addRole(adv_roles.n1)
+				} else if(cooldown[message.author.id][2] == 6) {
+					message.member.removeRole(adv_roles.n1)
+					message.member.addRole(adv_roles.n2)
+				} else if(cooldown[message.author.id][2] > 7) {
+					message.member.removeRole(adv_roles.n2)
+					message.member.addRole(adv_roles.muted)
+				}
+			}
+		}
+		console.log(cooldown[message.author.id])
+		if(cooldown[message.author.id][1] != 0) {
+			console.log("nope")
+			return;
+		}*/ 
 		let commandsName = client.commands.get(cmd.slice(PREFIX.length));
 		let aliasesName = client.commands.get(client.aliases.get(cmd.slice(PREFIX.length)));
 		let commandFile = commandsName || aliasesName;
 
 		if (commandFile) { 
-			msgR();
+			if(checkIt(cmd.slice(PREFIX.length))) { msgR(); }
 			commandFile.run(client, message, args);
 		} else {
 			message.channel.send(`Uh, ese no es un comando válido. Revisa los comandos con ${PREFIX}help.`);
 		}
 	}
-
-	/*const msgOriginal = message;
-	const msgLower = message.content.toLowerCase();
-
-	if (msgLower.startsWith('uwu')) {
-		message.channel.send("¡DON\'T UWU!");
-	};*/
 });
 
 /* Client */
