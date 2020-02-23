6ff2d05e44e7edd040fadef1730618155a12d8fd
Alexis Aldana Merlin <alexis.aldana@next-technologies.com.mx>
2020-02-23 02:21:52 -0600

    [fix] deployment

--- scripts/startEstiben.sh
+++ scripts/startEstiben.sh
@@ -2,9 +2,8 @@
 
 #Constantes que se usarán para arrancar a estiben
 logs_dir="/u01/log/Estiben/"
-log_date=$(date +'%d-%B-%Y-%H%M')
 estiben_directory="/home/centos/projects/scp/estiben-bot/"
-log="${logs_dir}estiben_${log_date}.log"
+log="${logs_dir}Estiben.log"
 process_name="Estiben"
 
 #pkill -f "${process_name}"
@@ -17,12 +16,14 @@ then
     sudo chown centos:centos "${logs_dir}"
 fi
 
+> "${log}"
+
 cd "${estiben_directory}"
 #Instala las dependencias de Node que sean necesarias
 npm install
 
 #Ejecuta el servicio en segundo plano y redirige la salida a un archibo de log
-nohup bash -c "exec -a ${process_name} node ${estiben_directory}index.js" > "${log}" &
+nohup bash -c "exec -a ${process_name} node ${estiben_directory}index.js" > "${log}" 2> /dev/null &
 
 echo "Servicio iniciado con el nombre ${process_name}"
 echo "Consulte el Log en: ${log}"
