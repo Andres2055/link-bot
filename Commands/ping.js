8a832ceb15ec1d8dd57b010f126f36fce1db3cf7
Merlin-VI <@merlinvi>
2020-10-28 14:32:35 -0600

    [add] ping

---
+++ Commands/ping.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = async(client, message, args) => {
+
+    message.channel.send("La Ley requiera que diga PONG. Aquí ando, no se espanten");
+}
+
+module.exports.config = {
+    name: "ping",
+    aliases: ["estatus", ],
+    activo: true,
+    configurable: true,
+    grupo: "GENERAL",
+    contador: 0
+}
